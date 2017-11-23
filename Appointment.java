ff1e30555a80b4f7dd4f7bc18ac5df8f33fb4c03
sturner6 <sturner6@sheffield.ac.uk>
2017-11-23 21:57:59 +0000

    Modified Appointment

--- Appointment.java
+++ Appointment.java
@@ -1,13 +1,25 @@
 import java.util.Date;
 
+public enum Prtner {
+    DENTIST, HYGIENIST
+}  
+
 public class Appointment {
 
 	private Date startTime;
 	private Date endTime;
+	private Prtner partner;
+	private int patientID;
+	private string nameOfTreatment;
+	private boolean seen; 
 
-	public Appointment(Date startTime, Date endTime) {
+	public Appointment(Date startTime, Date endTime, Prtner partner, int patientID, string nameOfTreatment, boolean seen ) {
 		this.startTime = startTime;
 		this.endTime = endTime;
+		this.partner = partner;
+		this.patientID = patientID;
+		this.nameOfTreatment = nameOfTreatment;
+		this.seen = seen;
 	}
 
 	public Date getStartTime() {
@@ -17,7 +29,23 @@ public class Appointment {
 	public Date getEndTime() {
 		return endTime;
 	}
+	
+	public Prtner getPartner() {
+		return partner;
+	}	
 
+	public int getPatientID() {
+		return patientID;
+	}	
+	
+	public string getNameOfTreatment() {
+		return nameOfTreatment;
+	}
+
+	public boolean getSeen() {
+		return seen;
+	}
+	
 	public void setStartTime(Date startTime) {
 		this.startTime =  startTime;
 	}
@@ -25,7 +53,23 @@ public class Appointment {
 	public void setEndTime(Date endTime) {
 		this.endTime =  endTime;
 	}
+	
+	public void setPartner(Prtner partner) {
+		this.partner =  partner;
+	}
 
+	public void setPatientID(int patientID) {
+		this.patientID =  patientID;
+	}
+	
+	public void setNameOfTreatment(string nameOfTreatment) {
+		this.nameOfTreatment =  nameOfTreatment;
+	}
+	
+	public void setSeen(boolean seen) {
+		this.seen =  seen;
+	}
+	
 	public String toString(){
 		String appointment;
 		appointment = "Start time: " + startTime + " End time: " + endTime;
