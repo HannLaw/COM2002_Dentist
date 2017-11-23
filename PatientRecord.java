4a18e8a3de76037301aa0c6672d593530570be05
Hannah Law <hllaw1@sheffield.ac.uk>
2017-11-23 17:38:52 +0000

    enums stuff added

--- PatientRecord.java
+++ PatientRecord.java
@@ -1,9 +1,17 @@
 import java.util.Date;
 
+public enum Ttl {
+  MR, MRS, MISS, MS, DR
+}
+
+public enum NoP {
+  NHS, MAINTENANCE, ORALHEALTH, DENTALREPAIR
+}
+
 public class PatientRecord {
 
-	private Enum title;
-	private Enum nameOfPlan;
+	private Ttl title;
+	private NoP nameOfPlan;
 	private String forename;
 	private String surname;
 	private String contactNumber;
@@ -14,116 +22,116 @@ public class PatientRecord {
 	private int remainingCheckUps;
 	private int remainingHygieneVisits;
 	private int remainingRepairs;
-	
+
 	public PatientRecord(Enum title, Enum nameOfPlan, String forename, String surname, String contactNumber, String houseNumber, String postCode, Date dateOfBirth, int patientID, int remainingCheckUps, int remainingHygieneVisits, int remainingRepairs) {
 		this.title = title;
-		this.nameOfPlan = nameOfPlan; 
-		this.forename = forename; 
-		this.surname = surname; 
-		this.contactNumber = contactNumber; 
+		this.nameOfPlan = nameOfPlan;
+		this.forename = forename;
+		this.surname = surname;
+		this.contactNumber = contactNumber;
 		this.houseNumber = houseNumber;
 		this.postCode = postCode;
-		this.dateOfBirth = dateOfBirth; 
-		this.patientID = patientID; 
-		this.remainingCheckUps = remainingCheckUps; 
+		this.dateOfBirth = dateOfBirth;
+		this.patientID = patientID;
+		this.remainingCheckUps = remainingCheckUps;
 		this.remainingHygieneVisits = remainingHygieneVisits;
-		this.remainingRepairs = remainingRepairs;		
+		this.remainingRepairs = remainingRepairs;
 	}
-	
+
 	public Enum getTitle() {
 		return title;
 	}
-	
+
 	public Enum getNameOfPlan() {
 		return nameOfPlan;
 	}
-	
+
 	public String getForename() {
 		return forename;
 	}
-	
+
 	public String getSurname() {
 		return surname;
 	}
-	
+
 	public String getContactNumber() {
 		return contactNumber;
 	}
-	
+
 	public String getHouseNumber() {
 		return houseNumber;
 	}
-	
+
 	public String getPostCode() {
 		return postCode;
 	}
-	
+
 	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
-	
+
 	public int getPatientID() {
 		return patientID;
 	}
-	
+
 	public int getRemainingCheckUps() {
 		return remainingCheckUps;
 	}
-	
+
 	public int getRemainingHygieneVisits() {
 		return remainingHygieneVisits;
 	}
-	
+
 	public int getRemainingRepairs() {
 		return remainingRepairs;
 	}
-	
+
 	public void setTitle(Enum title) {
 		this.title =  title;
 	}
-	
+
 	public void setNameOfPlan(Enum nameOfPlan) {
 		this.nameOfPlan =  nameOfPlan;
 	}
-	
+
 	public void setForename(String forename) {
 		this.forename =  forename;
 	}
-	
+
 	public void setSurname(String surname)) {
 		this.surname =  surname;
 	}
-	
+
 	public void setContactNumber(String contactNumber) {
 		this.contactNumber =  contactNumber;
 	}
-	
+
 	public void setHouseNumber(String houseNumber) {
 		this.houseNumber =  houseNumber;
 	}
-	
+
 	public void setPostCode(String postCode) {
 		this.postCode =  postCode;
 	}
-	
+
 	public void setDateOfBirth(Date dateOfBirth) {
 		this.dateOfBirth =  dateOfBirth;
 	}
-	
+
 	public void setPatientID(int patientID) {
 		this.patientID =  patientID;
 	}
-	
+
 	public void setRemainingCheckUps(int remainingCheckUps) {
 		this.remainingCheckUps =  remainingCheckUps;
 	}
-	
+
 	public void setRemainingHygieneVisits(int remainingHygieneVisits) {
 		this.remainingHygieneVisits =  remainingHygieneVisits;
 	}
-	
+
 	public void setRemainingRepairs(int remainingRepairs) {
 		this.remainingRepairs =  remainingRepairs;
 	}
-	
+
 }
