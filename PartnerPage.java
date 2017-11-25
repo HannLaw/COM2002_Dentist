11a7146386e8fcf452f07a0e1a2cba0fee89e2ce
Hannah Law <hllaw1@sheffield.ac.uk>
2017-11-25 18:24:07 +0000

    Partner page added

---
+++ PartnerPage.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class PartnerPage {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton button1;
+}
