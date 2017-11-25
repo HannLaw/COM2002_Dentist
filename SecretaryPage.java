a01e3b5c4a752356c3fd2b9a4587d59889b72af7
Hannah Law <hllaw1@sheffield.ac.uk>
2017-11-25 17:35:50 +0000

    Secretary swing interface prelim

---
+++ SecretaryPage.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class SecretaryPage {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton button1;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JButton BookButton;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JComboBox comboBox3;
+    private JTextField textField13;
+    private JTextField textField14;
+    private JTextField textField15;
+    private JTextField textField16;
+    private JTextField textField17;
+    private JButton submitButton;
+    private JTextField textField18;
+    private JTextField textField19;
+    private JTextField textField20;
+    private JButton submitButton1;
+}
