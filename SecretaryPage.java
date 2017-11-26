0e317d6c872fc17bf9829c1e039636ddb9ebfbd9
Hannah Law <hllaw1@sheffield.ac.uk>
2017-11-26 17:47:47 +0000

    Updated UI of secretary

--- SecretaryPage.java
+++ SecretaryPage.java
@@ -6,19 +6,22 @@ public class SecretaryPage {
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
-    private JButton button1;
     private JTextField textField4;
     private JTextField textField5;
     private JTextField textField6;
     private JTextField textField7;
     private JTextField textField8;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
-    private JButton BookButton;
     private JTextField textField9;
     private JTextField textField10;
     private JTextField textField11;
     private JTextField textField12;
+    private JTextField textField18;
+    private JTextField textField19;
+    private JTextField textField20;
+    private JComboBox comboBox1;
+    private JButton submitButton2;
+    private JButton submitButton3;
+    private JComboBox comboBox2;
     private JComboBox comboBox3;
     private JTextField textField13;
     private JTextField textField14;
@@ -26,8 +29,5 @@ public class SecretaryPage {
     private JTextField textField16;
     private JTextField textField17;
     private JButton submitButton;
-    private JTextField textField18;
-    private JTextField textField19;
-    private JTextField textField20;
     private JButton submitButton1;
 }
