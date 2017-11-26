089111dca969d2a2265b2332638e0cebf8de7926
Hannah Law <hllaw1@sheffield.ac.uk>
2017-11-26 17:18:31 +0000

    Intro screen added.

---
+++ IntroScreen.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class IntroScreen {
+    private JButton secretaryButton;
+    private JPanel panel1;
+    private JButton dentistButton;
+    private JButton hygienistButton;
+    private JTextArea sheffieldDentalCareTextArea;
+}
