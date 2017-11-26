c163a5e99b72edec7811e8b6c49415650313e78d
Hannah Law <hllaw1@sheffield.ac.uk>
2017-11-26 16:54:07 +0000

    View appointment popup added

---
+++ ViewAppt.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class ViewAppt {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JCheckBox patientSeenCheckBox;
+    private JTextField textField5;
+    private JCheckBox paidCheckBox;
+    private JCheckBox deferredCheckBox;
+    private JButton cancelButton;
+}
