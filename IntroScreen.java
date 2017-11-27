dfb2bb670d044b11459cc15f88ff084b74c2cf23
Hannah Law <hllaw1@sheffield.ac.uk>
2017-11-27 13:35:23 +0000

    IntroScreen buttons added

--- IntroScreen.java
+++ IntroScreen.java
@@ -1,8 +1,43 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class IntroScreen {
     private JButton secretaryButton;
     private JPanel panel1;
     private JButton dentistButton;
     private JButton hygienistButton;
+
+    public IntroScreen() {
+        secretaryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        SecretaryPage sFrame = new SecretaryPage();
+                    }
+                });
+            }
+        });
+        dentistButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        PartnerPage dFrame = new PartnerPage();
+                    }
+                });
+            }
+        });
+        hygienistButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        PartnerPage hFrame = new PartnerPage();
+                    }
+                });
+            }
+        });
+    }
 }
