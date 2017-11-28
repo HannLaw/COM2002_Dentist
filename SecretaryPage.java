231d3a1136e6477787456d1f75ca1c40dc4d9e18
sturner6 <sturner6@sheffield.ac.uk>
2017-11-28 13:11:09 +0000

    More Merging with D and H tables

--- SecretaryPage.java
+++ SecretaryPage.java
@@ -1,4 +1,3 @@
-//package com2002;
 import javax.swing.*;
 
 import java.awt.event.ActionEvent;
@@ -48,7 +47,8 @@ public class SecretaryPage extends JFrame {
     private JButton editPatientSubmit;
     private JPanel dentistCal;
     private JPanel hygienistCal;
-    private JTable Calender;
+    private JTable CalenderD;
+	private JTable CalenderH;
 
     public SecretaryPage() {
 
@@ -219,11 +219,10 @@ public class SecretaryPage extends JFrame {
         }
 	}
 
-	public void viewAppointments(Date d) throws SQLException,Exception { 
+	public void viewDAppointments(Date d) throws SQLException,Exception { 
 		try { 
 			
 			createConnection();
-			
 			
 			int year = d.getYear();
 			int month = d.getMonth();
@@ -233,34 +232,7 @@ public class SecretaryPage extends JFrame {
 			statement = connect.createStatement();
 			
 			//Find out how many days are in the month we are in. 
-			 switch (month+1) {
-	            case 1 :
-	            case 3:
-	            case 5:
-	            case 7:
-	            case 8:
-	            case 10:
-	            case 12:
-	                lastDate = 31;
-	                break;
-	            case 4:
-	            case 6:
-	            case 9:
-	            case 11:
-	                lastDate = 30;
-	                break;
-	            case 2:
-	                if (((year % 4 == 0) && 
-	                     !(year % 100 == 0))
-	                     || (year % 400 == 0))
-	                    lastDate = 29;
-	                else
-	                    lastDate = 28;
-	                break;
-	            default:
-	                System.out.println("Invalid month.");
-	                break;
-	        }
+			lastDate = getLastDate(month,year); 
 			//Work out how many days are in next month. 
 			if  ( (date+6) > lastDate ){
 					numDaysNxtMonth = (date+6) - lastDate; 
@@ -287,14 +259,14 @@ public class SecretaryPage extends JFrame {
 			if (numDaysNxtMonth == 0) {
 
 				resultSet1 = statement
-					.executeQuery("SELECT * FROM dentistry.appointments WHERE dateOfAppointment BETWEEN '"+ year +
+					.executeQuery("SELECT * FROM dentistry.appointments WHERE partner = 'Dentitst' AND dateOfAppointment BETWEEN '"+ year +
 							"-" + (month+1) + "-"+ date + "' AND '" + year + "-" + (month+1) + "-" + (date+7) + "';");
 
 			}
 			else {
 				
 				resultSet1 = statement
-						.executeQuery("SELECT * FROM dentistry.appointments WHERE dateOfAppointment BETWEEN '"+ year +
+						.executeQuery("SELECT * FROM dentistry.appointments WHERE partner = 'Dentitst' AND dateOfAppointment BETWEEN '"+ year +
 								"-" + (month+1) + "-"+ date + "' AND '" + year + "-" + (month + 2) + "-" + numDaysNxtMonth + "';");
 				
 			}
@@ -329,7 +301,101 @@ public class SecretaryPage extends JFrame {
 	            aptArray[i][6] = b; 
 	            i ++; 
 	        }
-			Calender = new JTable(aptArray,(Object[]) columns());
+			CalenderD = new JTable(aptArray,(Object[]) columns());
+			if (resultSet1 != null) {
+                resultSet1.close();
+            }
+		}catch (Exception e) {
+            throw e;
+        } finally {
+            close();
+        }
+	}
+	
+	public void viewHAppointments(Date d) throws SQLException,Exception { 
+		try { 
+			
+			createConnection();
+			
+			int year = d.getYear();
+			int month = d.getMonth();
+			int date = d.getDate();
+			int lastDate = 0;
+			int numDaysNxtMonth;
+			statement = connect.createStatement();
+			
+			//Find out how many days are in the month we are in. 
+			lastDate = getLastDate(month,year);
+			 
+			//Work out how many days are in next month. 
+			if  ( (date+6) > lastDate ){
+					numDaysNxtMonth = (date+6) - lastDate; 
+			}
+			else numDaysNxtMonth = 0; 
+			
+			//System.out.println(numDaysNxtMonth);
+			
+			//Use numDaysNxtMonth to execute the appropriate query.
+			ResultSet resultSet2 = statement
+					.executeQuery("SELECT COUNT(*) AS count FROM dentistry.appointments WHERE dateOfAppointment BETWEEN '"+ year +
+							"-" + (month+1) + "-"+ date + "' AND '" + year + "-" + (month + 2) + "-" + numDaysNxtMonth + "';");
+            int amountOfAppointments=0;
+            while(resultSet2.next()){ 
+            	
+            	amountOfAppointments = resultSet2.getInt("count"); 
+            
+            }
+			if (resultSet2 != null) {
+                resultSet2.close();
+            }
+			
+			ResultSet resultSet1 = null;
+			if (numDaysNxtMonth == 0) {
+
+				resultSet1 = statement
+					.executeQuery("SELECT * FROM dentistry.appointments WHERE partner = 'Hygienist' AND dateOfAppointment BETWEEN '"+ year +
+							"-" + (month+1) + "-"+ date + "' AND '" + year + "-" + (month+1) + "-" + (date+7) + "';");
+
+			}
+			else {
+				
+				resultSet1 = statement
+						.executeQuery("SELECT * FROM dentistry.appointments WHERE partner = 'Hygienist' AND dateOfAppointment BETWEEN '"+ year +
+								"-" + (month+1) + "-"+ date + "' AND '" + year + "-" + (month + 2) + "-" + numDaysNxtMonth + "';");
+				
+			}
+			 
+            String[][] aptArray = new String[amountOfAppointments+1][7];
+			int i = 0;
+			while (resultSet1.next()) {
+
+				Date date0 = resultSet1.getDate("dateOfAppointment");
+	            Time stime = resultSet1.getTime("startTime");
+	            Time etime = resultSet1.getTime("endTime");
+	            String partner = resultSet1.getString("partner");
+	            int pID = resultSet1.getInt("patientID");
+	            String trtmnt = resultSet1.getString("treatmentName");
+	            boolean seen = resultSet1.getBoolean("seen");
+	            
+	            DateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+	            String date1 = sdf.format(date0);
+	            DateFormat time1 = new SimpleDateFormat("HH.mm.ss");
+	            String t1 = time1.format(stime);
+	            DateFormat time2 = new SimpleDateFormat("HH.mm.ss");
+	            String t2 = time2.format(etime);
+	            String pID2 = Integer.toString(pID);
+	            String b = Boolean.toString(seen);
+	        	
+	            aptArray[i][0] = date1;
+	            aptArray[i][1] = t1;
+	            aptArray[i][2] = t2; 
+	            aptArray[i][3] = partner; 
+	            aptArray[i][4] = pID2; 
+	            aptArray[i][5] = trtmnt; 
+	            aptArray[i][6] = b; 
+	            i ++; 
+	        }
+			CalenderH = new JTable(aptArray,(Object[]) columns());
 			if (resultSet1 != null) {
                 resultSet1.close();
             }
@@ -343,6 +409,41 @@ public class SecretaryPage extends JFrame {
     public Object[] columns() {
         Object[] column = {"Date","Start","End","Partner","PatientID","Treatment","Seen"};
         return column;
+    }
+	
+	public int getLastDate(int month, int year) { 
+    	int lastDate = 30;
+    	switch (month+1) {
+        case 1 :
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            lastDate = 31;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            lastDate = 30;
+            break;
+        case 2:
+            if (((year % 4 == 0) && 
+                 !(year % 100 == 0))
+                 || (year % 400 == 0))
+                lastDate = 29;
+            else
+                lastDate = 28;
+            break;
+        default:
+            System.out.println("Invalid month.");
+            break;
+        }
+    	
+        return lastDate;
+        
     }
  
 	public void cancelAppointment(int PatientID, Prtner p) throws Exception { 
