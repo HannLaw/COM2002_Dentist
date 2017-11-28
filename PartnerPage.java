9203e8e552266a6f3009642e553ee49e906a6cb6
Hannah Law <hllaw1@sheffield.ac.uk>
2017-11-28 14:14:58 +0000

    partner cal stuff added

--- PartnerPage.java
+++ PartnerPage.java
@@ -1,10 +1,74 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-public class PartnerPage {
+public class PartnerPage extends  JFrame {
     public JPanel partPagePanel;
     private JTabbedPane tabbedPane1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JButton submitButton;
+    private JTextField patientID;
+    private JTextField firstName;
+    private JTextField surname;
+    private JButton viewApptSubmit;
+    private JTextField dayDate;
+    private JPanel dayCal;
+    private JTextField weekDate;
+    private JFrame j = this;
+    private String t = j.getTitle();
+
+
+    public PartnerPage() {
+        SecretaryPage sec = new SecretaryPage();
+
+        dayDate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (t=="DentistPage") {
+                    try {
+                        sec.viewDAppointments(getDate(dayDate));
+                    } catch (Exception c) {
+                        System.out.println("Uh oh...");
+                    }
+                }
+                else {
+                    try {
+                        sec.viewHAppointments(getDate(dayDate));
+                    } catch (Exception c) {
+                        System.out.println("Uh oh...");
+                    }
+                }
+            }
+        });
+        weekDate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (t=="DentistPage") {
+                    try {
+                        sec.viewDAppointments(getDate(weekDate));
+                    } catch (Exception c) {
+                        System.out.println("Uh oh...");
+                    }
+                }
+                else {
+                    try {
+                        sec.viewHAppointments(getDate(weekDate));
+                    } catch (Exception c) {
+                        System.out.println("Uh oh...");
+                    }
+                }
+            }
+        });
+    }
+
+    public Date getDate(JTextField d) {
+        String dDate = d.getText();
+        int yearD = Integer.valueOf(dDate.substring(6));
+        int monthD = Integer.valueOf(dDate.substring(3,4));
+        int dayD = Integer.valueOf(dDate.substring(0,1));
+        Date dateD = new Date(yearD,monthD,dayD);
+        return dateD;
+    }
 }
